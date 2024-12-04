92cd951793020ea3b4ec1faf4d27675a700056d8
ROTY Jeremy <rotyjeremy@gmail.com>
2024-12-04 11:46:48 +0100

    Set users connexion database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import 'dotenv/config'
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: Number(process.env.DATABASE_PORT) || 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DBNAME,
+      autoLoadModels: true,
+      synchronize: Boolean(process.env.PRODUCTION)
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
