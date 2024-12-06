7c0b344735e4d9afb7e2fa36187b825c4e0830d6
ROTY Jeremy <rotyjeremy@gmail.com>
2024-12-06 11:09:28 +0100

    Update task controller and service search

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,14 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { Task } from './task.model';
 
 @Controller('task')
 export class TaskController {
-    constructor(private taskService:TaskService){
-
-    }
-    @Post('addTask')
-    async register(@Body() taskData: Partial<Task>){
-      return this.taskService.create(taskData);
-    }
+  constructor(private taskService: TaskService) {}
+  @Post('addTask')
+  async register(@Body() taskData: Partial<Task>) {
+    return this.taskService.create(taskData);
+  }
+  @Get('get')
+  async getAllTasks(): Promise<Task[]> {
+    return this.taskService.getAllTasks();
+  }
+  @Get('search')
+  async searchTasks(@Query('title') title: string): Promise<Task[]> {
+    return this.taskService.searchAllTasks(title);
+  }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,10 +1,13 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { Task } from './task.model';
 import { InjectModel } from '@nestjs/sequelize';
+import { Op } from 'sequelize';
 
 @Injectable()
 export class TaskService {
   constructor(@InjectModel(Task) private taskModel: typeof Task) {}
+
+  
   async create(taskData: Partial<Task>): Promise<Task> {
     const existingTask = await this.taskModel.findOne({
       where: { title: taskData.title },
@@ -13,5 +16,18 @@ export class TaskService {
       throw new ConflictException('A task with this title already exists.');
     }
     return this.taskModel.create(taskData);
+  }
+
+  async getAllTasks(): Promise<Task[]> {
+    return this.taskModel.findAll();
+  }
+  async searchAllTasks(inTitle: string): Promise<Task[]> {
+    return this.taskModel.findAll({
+      where: {
+        title: {
+          [Op.like]: `%${inTitle}%`, // Recherche partielle sur le titre
+        },
+      },
+    });
   }
 }
